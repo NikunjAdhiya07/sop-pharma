38221d329cb7fe67ab2c3011d2994a72585be944
VedantRaval3011 <vedantraval30@gmail.com>
2026-05-23 15:56:36 +0530

    chore: remove debug scripts

--- check_sops.js
+++
@@ -1,7 +0,0 @@
-const http = require('http');
-
-http.get('http://localhost:3000/api/dashboard/sops', (res) => {
-  let data = '';
-  res.on('data', (chunk) => data += chunk);
-  res.on('end', () => console.log(JSON.parse(data).data.filter(d => d.sopNo === 'PEGE23-02')));
-});
